5538da817700dd0d096a153421b25a288bcbc821
Kavinramasamy <kavinramasamymech@gmail.com>
2023-12-19 15:50:29 +0530

    updateproject

--- src/Component/Project.js
+++ src/Component/Project.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import rental_img from './image/pic.png'
 
 const Project = () => {
   return (
-    <div style={{  backgroundColor: "black" }}> <h4 className='active text-light'>Project</h4>
+    <div style={{ backgroundColor: "black" }}> <h4 className='active text-light'>Project</h4>
       <div className='container'>
         <div className='row'>
           <div class="card col-md-4 p-3 mx-3 mb-3 " style={{ width: "18rem" }}>
@@ -10,17 +11,19 @@ const Project = () => {
             <div class="card-body">
               <h5 class="card-title text-dark">Brewery List</h5>
               <p class="card-text active text-dark">You can find the brewery shop address and contact using this application.</p>
-              <a href="https://marvelous-brioche-d91299.netlify.app/" class="btn btn-success mb-2" style={{width:"100%"}}>Visit</a>
-              <a href="https://github.com/Kavinramasamy/brewery-lists" class="btn btn-success mb-2" style={{width:"100%"}}>Front-end source code</a>
-              <a href="" class="btn btn-danger disabled" style={{width:"100%"}}>Back-end source code</a>
+              <a href="https://marvelous-brioche-d91299.netlify.app/" class="btn btn-success mb-2" style={{ width: "100%" }}>Visit</a>
+              <a href="https://github.com/Kavinramasamy/brewery-lists" class="btn btn-success mb-2" style={{ width: "100%" }}>Front-end source code</a>
+              <a href="" class="btn btn-danger disabled" style={{ width: "100%" }}>Back-end source code</a>
             </div>
           </div>
           <div class="card col-md-4 p-3 mx-3 mb-3" style={{ width: "18rem" }}>
+            <img src={rental_img} alt='rental-image' />
             <div class="card-body">
-              <h5 class="card-title">Project title</h5>
-              <p class="card-text">write some note about Project.</p>
-              <a href="#" class="btn btn-primary">Button</a>
-            </div>
+              <h5 class="card-title">Rental products</h5>
+              <p class="card-text">You can use this website to take and buy a products for rent with affordable price.</p>
+              <a href="https://frontend-hackton-guvi.vercel.app/" class="btn btn-success mb-2" style={{ width: "100%" }}>Visit</a>
+              <a href="https://github.com/Kavinramasamy/frontend-hackton-guvi" class="btn btn-success mb-2" style={{ width: "100%" }}>Front-end source code</a>
+              <a href="https://github.com/Kavinramasamy/backend--hackathon-project" class="btn btn-danger" style={{ width: "100%" }}>Back-end source code</a>            </div>
           </div>
           <div class="card col-md-4 p-3 mx-3 mb-3" style={{ width: "18rem" }}>
             <div class="card-body">
@@ -30,9 +33,8 @@ const Project = () => {
             </div>
           </div>
 
-
         </div>
-        </div>
+      </div>
     </div>
   )
 }
